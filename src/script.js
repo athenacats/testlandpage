3f177ea43ee0d2088fbc06fe3c062a7db34449e1
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-16 21:08:10 +0300

    Further css

---
+++ src/script.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-plusplus */
+let slidePosition = 1;
+
+function SlideShow(n) {
+  let i;
+  const slides = document.getElementsByClassName("info");
+  const circles = document.getElementsByClassName("ellipse");
+  if (n > slides.length) {
+    slidePosition = 1;
+  }
+  if (n < 1) {
+    slidePosition = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].classList.remove("enable");
+  }
+  slides[slidePosition - 1].style.display = "flex";
+  circles[slidePosition - 1].classList.add("enable");
+}
+
+function currentSlide(n) {
+  SlideShow((slidePosition = n));
+}
